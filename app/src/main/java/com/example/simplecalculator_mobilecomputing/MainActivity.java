a7549b9bcd6a62d309d077b2ca0416a940160de8
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2021-10-23 19:29:44 +0500

    Added the Functionality

--- app/src/main/java/com/example/simplecalculator_mobilecomputing/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator_mobilecomputing/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.simplecalculator_mobilecomputing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSub,btnMul,btnDiv,btnEqual,btnDot;
+    Button btnClear;
+    EditText ed1;
+    float Res1,Res2;
+    boolean Add,Sub,Mul,Div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+
     }
 }
